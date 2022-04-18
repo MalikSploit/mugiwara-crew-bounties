0951db9741cfe44ba9cf2c9a0984e367da734523
MalikSploit <maksucce@gmail.com>
2022-04-18 05:32:54 +0200

    init 2

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({name, bounty, id}) =>
 {
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='' src={`https://raw.githubusercontent.com/MalikSploit/Mugiwara/main/images/${id}.jpg?`}/>
+			<img alt='' src={`https://raw.githubusercontent.com/MalikSploit/mugiwara-crew-bounties/master/images/${id}.jpg?`}/>
 			<div>
 				<h2>{name}</h2>
 				<p>{bounty}</p>
